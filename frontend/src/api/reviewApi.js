7a7e3af4436a0909310d5993f714385f4c5142bd
AvneshTech <avneshbaghel.30july@gmail.com>
2026-07-06 22:28:46 +0530

    Only authorize persons can give the revies

--- frontend/src/api/reviewApi.js
+++ frontend/src/api/reviewApi.js
@@ -1,10 +1,10 @@
 import api from "./axiosInstance.js";
 
-// export const createReview = (listingId, data) =>
-//   api.post(`/listings/${listingId}/reviews`, data);
+export const createReview = (listingId, data) =>
+  api.post(`/listings/${listingId}/reviews`, data);
 
-// export const updateReview = (listingId, reviewId, data) =>
-//   api.put(`/listings/${listingId}/reviews/${reviewId}`, data);
+export const updateReview = (listingId, reviewId, data) =>
+  api.put(`/listings/${listingId}/reviews/${reviewId}`, data);
 
-// export const deleteReview = (listingId, reviewId) =>
-//   api.delete(`/listings/${listingId}/reviews/${reviewId}`);
+export const deleteReview = (listingId, reviewId) =>
+  api.delete(`/listings/${listingId}/reviews/${reviewId}`);
