5bef5a13c189da35c0714e457194ae95637a188f
AvneshTech <avneshbaghel.30july@gmail.com>
2026-07-06 22:22:43 +0530

    Auth Api single checking

--- frontend/src/api/authApi.js
+++ frontend/src/api/authApi.js
@@ -1,6 +1,6 @@
 import api from "./axiosInstance.js";
 
 export const signupRequest = (data) => api.post("/auth/signup", data);
-export const loginRequest = (data) => api.post("/auth/login", data);
-export const logoutRequest = () => api.post("/auth/logout");
-export const getMe = () => api.get("/auth/me");
+// export const loginRequest = (data) => api.post("/auth/login", data);
+// export const logoutRequest = () => api.post("/auth/logout");
+// export const getMe = () => api.get("/auth/me");
