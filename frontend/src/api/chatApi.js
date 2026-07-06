fce00d450ea15e81deb168d19f82266e5117eb45
AvneshTech <avneshbaghel.30july@gmail.com>
2026-07-06 22:27:02 +0530

    Comments add in chat.js file to remainder checking

--- frontend/src/api/chatApi.js
+++ frontend/src/api/chatApi.js
@@ -1,5 +1,7 @@
 import api from "./axiosInstance.js";
-
+// Check the api.js file for the base URL and other configurations
+// All the API requests related to chat functionality will be defined here
+// All api check later it is not working properly
 export const fetchChatHistory = () => api.get("/chat/history");
 export const sendChatMessage = (message) => api.post("/chat", { message });
 export const clearChat = () => api.post("/chat/clear");
